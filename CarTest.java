e49ab1989380f6e7ca96bab4e9ea3b560526c557
Jakob Wennberg <jakob.wennberg@gmail.com>
2025-01-22 10:44:26 +0100

    Added tests using JUnit jupiter

---
+++ CarTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarTest {
+
+    @Test
+    void testCarMovement() {
+        Car volvo = new Volvo240();
+        volvo.startEngine();
+        volvo.move();
+        assertEquals(0.1, volvo.getY(), 0.01);
+    }
+
+    @Test
+    void testGas() {
+        Car saab = new Saab95();
+        saab.startEngine();
+        saab.gas(0.5);
+        assertTrue(saab.getCurrentSpeed() > 0.1);
+    }
+
+    @Test
+    void testTurn() {
+        Car volvo = new Volvo240();
+        volvo.turnRight();
+        assertEquals(90, volvo.getDirection());
+        volvo.turnLeft();
+        assertEquals(0, volvo.getDirection());
+    }
+}
